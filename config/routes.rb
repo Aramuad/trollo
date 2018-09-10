ea8d62810f23e61485ca36efbe7f369389330e5a
Nichol Blackburn <nicholblackburn@outlook.com>
2018-09-10 16:11:09 -0600

    routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  
-  root 'boards#index'
-
-  get 'tasks/index'
-  get 'tasks/show'
-  get 'tasks/new'
-
-  get 'boards/index'
-  get 'boards/show'
-  get 'boards/new'
-=======
 
   
->>>>>>> finish lists controller
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
