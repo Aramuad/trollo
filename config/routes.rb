683c1f8f89741d64bf715047c3f9c1b839efed55
Nichol Blackburn <nicholblackburn@outlook.com>
2018-09-10 17:24:09 -0600

    new_task form

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-=======
-Rails.application.routes.draw do
-<<<<<<< HEAD
->>>>>>> d65f9ca076b9358619187694eff1eaf844aea0fc
 
   Rails.application.routes.draw do
   
-<<<<<<< HEAD
     root 'boards#index'
   
     get 'tasks/index'
@@ -16,20 +10,15 @@ Rails.application.routes.draw do
     get 'boards/index'
     get 'boards/show'
     get 'boards/new'
-=======
-<<<<<<< HEAD
-  
-=======
-=======
-  
-  root 'boards#index'
+
+    get 'lists/index'
+    get 'lists/show'
+    get 'lists/new'
 
   resources :boards
   resources :lists
   resources :tasks
->>>>>>> modify routes
->>>>>>> ab2a59529f316f8c1324ad889b4407e02fc36b7a
->>>>>>> d65f9ca076b9358619187694eff1eaf844aea0fc
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
