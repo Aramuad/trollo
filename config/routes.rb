9f30079bbc3a3945c1af2a95ccffe612e0f69767
Nichol Blackburn <nicholblackburn@outlook.com>
2018-09-10 16:27:39 -0600

    tasks controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
-Rails.application.routes.draw do
 
+  Rails.application.routes.draw do
   
+    root 'boards#index'
+  
+    get 'tasks/index'
+    get 'tasks/show'
+    get 'tasks/new'
+  
+    get 'boards/index'
+    get 'boards/show'
+    get 'boards/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
