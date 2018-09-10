ab2a59529f316f8c1324ad889b4407e02fc36b7a
Aramuad <samcballard@gmail.com>
2018-09-10 16:26:21 -0600

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
 
   
+=======
+  
+  root 'boards#index'
+
+  resources :boards
+  resources :lists
+  resources :tasks
+>>>>>>> modify routes
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
