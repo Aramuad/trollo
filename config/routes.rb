2c0266f3577d61c0ddd2c66ceab26326783569f4
Aramuad <samcballard@gmail.com>
2018-09-10 15:24:42 -0600

    add lists controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/create'
+  get 'lists/edit'
+  get 'lists/update'
+  get 'lists/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,11 +1,8 @@
 FactoryBot.define do
   factory :task do
-<<<<<<< HEAD
     name { "MyString" }
     body { "MyString" }
     comment { "MyString" }
-=======
     
->>>>>>> bf7aaf9c9f40e7a107064a3153e42fe9a75e8f58
   end
 end
