ad7fca3972ffc4909b9b221f977292c86bd0515c
Nichol Blackburn <nicholblackburn@outlook.com>
2018-09-11 09:14:49 -0600

    fix merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,7 @@
 
-<<<<<<< HEAD
-  Rails.application.routes.draw do
-  
-    root 'boards#index'
-  
-    get 'tasks/index'
-    get 'tasks/show'
-    get 'tasks/new'
-  
-    get 'boards/index'
-    get 'boards/show'
-    get 'boards/new'
-
-    get 'lists/index'
-    get 'lists/show'
-    get 'lists/new'
-=======
 Rails.application.routes.draw do
-  
   root 'boards#index'
->>>>>>> 003fd62eebb452fc639d8763e787d5a9486a2222
-
   resources :boards
   resources :lists
   resources :tasks
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 003fd62eebb452fc639d8763e787d5a9486a2222
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
